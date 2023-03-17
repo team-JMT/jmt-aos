cda62b2601cacb6b358bbfd711c1313c54d29ec1
soopeach <hsjeon584@gmail.com>
2023-03-17 19:06:11 +0900

    [setting/module]: data Model & data Source 명칭 변경
    Co-authored-by: dogdduddy <dogdduddy@gmail.com>

--- app/src/main/java/org/gdsc/jmt/viewmodel/LoginViewModel.kt
+++ app/src/main/java/org/gdsc/jmt/viewmodel/LoginViewModel.kt
@@ -5,14 +5,14 @@ import android.app.PendingIntent
 import android.content.Intent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import org.gdsc.jmt.model.Data
+import org.gdsc.jmt.model.LoginDataSourceImpl
 import org.gdsc.jmt.view.GoogleLoginManager
 import com.google.android.gms.auth.api.identity.SignInCredential
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class LoginViewModel: ViewModel() {
-    private val data = Data()
+    private val loginDataSource = LoginDataSourceImpl()
     private val googleLoginManager = GoogleLoginManager()
 
     suspend fun googleLogin(activity: Activity):PendingIntent {
@@ -25,7 +25,7 @@ class LoginViewModel: ViewModel() {
     fun postGoogleToken(token:String?) {
         viewModelScope.launch {
             val deferred = async {
-                data.postGoogleToken(token)
+                loginDataSource.postGoogleToken(token)
             }
             deferred.await()
         }
@@ -34,7 +34,7 @@ class LoginViewModel: ViewModel() {
     fun postAppleToken(email:String, clientId:String) {
         viewModelScope.launch {
             val deferred = async {
-                data.postAppleToken(email, clientId)
+                loginDataSource.postAppleToken(email, clientId)
             }
             deferred.await()
         }
