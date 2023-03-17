34144314e730498cbe6eb349f05e8c737ac7b5a7
soopeach <hsjeon584@gmail.com>
2023-03-17 22:11:27 +0900

    [setting/module]: GoogleLoginManager 이름 변경
    Co-authored-by: dogdduddy <dogdduddy@gmail.com>

--- app/src/main/java/org/gdsc/jmt/viewmodel/LoginViewModel.kt
+++ app/src/main/java/org/gdsc/jmt/viewmodel/LoginViewModel.kt
@@ -6,22 +6,23 @@ import android.content.Intent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import org.gdsc.jmt.model.LoginDataSourceImpl
-import org.gdsc.jmt.view.GoogleLoginManager
+import org.gdsc.jmt.view.LoginManager
 import com.google.android.gms.auth.api.identity.SignInCredential
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class LoginViewModel: ViewModel() {
     private val loginDataSource = LoginDataSourceImpl()
-    private val googleLoginManager = GoogleLoginManager()
+    private val loginManager = LoginManager()
 
     suspend fun googleLogin(activity: Activity):PendingIntent {
-        return googleLoginManager.signInIntent(activity)
+        return loginManager.signInIntent(activity)
     }
 
-    fun getCredential(intent: Intent) :SignInCredential {
-        return googleLoginManager.oneTapClient.getSignInCredentialFromIntent(intent)
+    fun getGoogleCredential(intent: Intent) :SignInCredential {
+        return loginManager.oneTapClient.getSignInCredentialFromIntent(intent)
     }
+
     fun postGoogleToken(token:String?) {
         viewModelScope.launch {
             val deferred = async {
