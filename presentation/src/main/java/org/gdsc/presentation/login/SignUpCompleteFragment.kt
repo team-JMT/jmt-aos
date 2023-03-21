4817a62a8790c48e3d8c255bad5d0357e3ccb0d6
soopeach <hsjeon584@gmail.com>
2023-03-21 11:26:56 +0900

    [feat/sign_up_page]: 회원가입 완료 화면 추가

--- presentation/src/main/java/org/gdsc/presentation/login/SignUpCompleteFragment.kt
+++ presentation/src/main/java/org/gdsc/presentation/login/SignUpCompleteFragment.kt
@@ -5,37 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import org.gdsc.presentation.databinding.FragmentSignUpNicknameBinding
+import androidx.navigation.fragment.navArgs
+import org.gdsc.presentation.databinding.FragmentSignUpCompleteBinding
 
-class SignUpNicknameFragment : Fragment() {
+class SignUpCompleteFragment : Fragment() {
 
-    private var _binding: FragmentSignUpNicknameBinding? = null
+    private var _binding: FragmentSignUpCompleteBinding? = null
     private val binding
         get() = requireNotNull(_binding)
+
+    private val args: SignUpCompleteFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSignUpNicknameBinding.inflate(inflater, container, false)
+        _binding = FragmentSignUpCompleteBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setNextButton()
-    }
 
-    private fun setNextButton() {
-        binding.nextBtn.setOnClickListener {
-            val action = SignUpNicknameFragmentDirections.actionSignUpNicknameFragmentToSignUpCompleteFragment(binding.nicknameEditText.text)
-            findNavController().navigate(action)
-        }
+        binding.nicknameText.text = args.nickname
+
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
+
 }
