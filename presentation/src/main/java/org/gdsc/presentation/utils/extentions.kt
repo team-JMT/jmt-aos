a94d914c834c661d0d92703736633f26416ff6cb
soopeach <hsjeon584@gmail.com>
2023-03-21 16:07:41 +0900

    [feat/sign_up_page]: 닉네임 입력창 외부를 누르면 키보드 내려가는 기능 추가

--- presentation/src/main/java/org/gdsc/presentation/utils/extentions.kt
+++ presentation/src/main/java/org/gdsc/presentation/utils/extentions.kt
@@ -1,7 +1,10 @@
 package org.gdsc.presentation.utils
 
+import android.app.Activity
+import android.content.Context
 import android.os.Build
 import android.util.DisplayMetrics
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 
 val Fragment.deviceMetrics
@@ -32,3 +35,8 @@ val Fragment.deviceMetrics
         }
         displayMetrics
     }
+
+fun Fragment.hideKeyBoard(activity: Activity) {
+    val inputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(activity.currentFocus?.windowToken, 0)
+}
