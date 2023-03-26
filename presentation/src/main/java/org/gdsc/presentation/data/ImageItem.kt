627f38ef2f716ca194e701cbbefc4fc7921ae078
dogdduddy <dogdduddy@gmail.com>
2023-03-26 23:29:56 +0900

    [sign/profile]: Presentation Layer Model Domain에서 받아오기

--- presentation/src/main/java/org/gdsc/presentation/data/ImageItem.kt
+++
@@ -1,8 +0,0 @@
-package org.gdsc.presentation.data
-
-import android.net.Uri
-
-data class ImageItem(
-    var uri: String,
-    var bucket: String
-)
--- presentation/src/main/java/org/gdsc/presentation/viewmodel/ImagePickerViewModel.kt
+++ presentation/src/main/java/org/gdsc/presentation/viewmodel/ImagePickerViewModel.kt
@@ -1,9 +1,7 @@
 package org.gdsc.presentation.viewmodel
 
 import android.net.Uri
-import android.provider.MediaStore
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,17 +9,11 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.forEach
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import org.gdsc.domain.model.ImageItem
 import org.gdsc.domain.usecase.GetGalleryUseCase
-import org.gdsc.presentation.data.ImageItem
 import java.io.File
 import javax.inject.Inject
 
