896837bf8acd3d44c085543664980154d535e9e5
dogdduddy <dogdduddy@gmail.com>
2023-03-26 19:33:20 +0900

    [sign/profile]: 주석 & 불필요한 코드제거

--- data/src/main/java/org/gdsc/data/datasource/LoginDataSourceImpl.kt
+++ data/src/main/java/org/gdsc/data/datasource/LoginDataSourceImpl.kt
@@ -1,17 +1,9 @@
 package org.gdsc.data.datasource
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.net.Uri
-import android.os.Build
-import android.provider.MediaStore
-import android.util.Log
-import dagger.hilt.android.qualifiers.ApplicationContext
 import org.gdsc.data.network.LoginAPI
 import org.gdsc.domain.model.AppleLoginRequest
 import org.gdsc.domain.model.GoogleLoginRequest
 import org.gdsc.domain.model.LoginResponse
-import java.io.File
 import javax.inject.Inject
 
 
