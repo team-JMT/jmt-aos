c95d0f5ca82a97347d8cc5849fb630b446a2251e
soopeach <hsjeon584@gmail.com>
2023-04-02 23:17:43 +0900

    [feat/bottom_navigation_view]: bottom navigation view에 기본 항목들 연결

---
+++ presentation/src/main/java/org/gdsc/presentation/view/mypage/MyPageFragment.kt
@@ -0,0 +1,29 @@
+package org.gdsc.presentation.view.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.gdsc.presentation.databinding.FragmentMyPageBinding
+
+class MyPageFragment : Fragment() {
+
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+
+}
