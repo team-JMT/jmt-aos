2a15a7f16ce9cb8ea69c00bc28814bbdfa132311
soopeach <hsjeon584@gmail.com>
2023-04-04 22:30:33 +0900

    [feat/naver_map_init]: 둘러보기 페이지에 네이버 맵(mapView) 사용

--- presentation/src/main/java/org/gdsc/presentation/view/home/HomeFragment.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/home/HomeFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.naver.maps.map.MapView
 import org.gdsc.presentation.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +23,50 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync { map ->
+            map.uiSettings.isZoomControlEnabled = false
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+        mapView.onDestroy()
     }
 }
