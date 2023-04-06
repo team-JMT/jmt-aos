416a3d50168d4df3281ac51603b49a8e2af47a49
soopeach <hsjeon584@gmail.com>
2023-04-06 14:39:57 +0900

    [feat/jmt_search_edit_text]: addAfterTextChangedListener 확장 함수로 분리

--- presentation/src/main/java/org/gdsc/presentation/view/custom/JmtEditText.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/custom/JmtEditText.kt
@@ -2,13 +2,12 @@ package org.gdsc.presentation.view.custom
 
 import androidx.constraintlayout.widget.ConstraintLayout
 import android.content.Context
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import org.gdsc.presentation.R
 import org.gdsc.presentation.databinding.JmtEditTextBinding
+import org.gdsc.presentation.utils.addAfterTextChangedListener
 
 // TODO: 공백 처리 정책
 class JmtEditText(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
@@ -50,22 +49,8 @@ class JmtEditText(context: Context, attrs: AttributeSet) : ConstraintLayout(cont
         }
     }
 
-    fun addAfterTextChangedListener(block: (String) -> Unit) {
-        binding.nicknameEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                block(s.toString())
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-        })
-    }
-
     private fun setNicknameEditTextWatcher() {
-        addAfterTextChangedListener { checkNickname() }
+        binding.nicknameEditText.addAfterTextChangedListener { checkNickname() }
     }
 
     private fun hideVerifyLayout() {
