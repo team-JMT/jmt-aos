512eaa7b9034e92b4c70ad8e6f90dbac3ca45c4c
soopeach <hsjeon584@gmail.com>
2023-04-06 15:13:46 +0900

    [feat/jmt_search_edit_text]: JmtSearchEditText 아이콘, 취소 버튼 visibility 구현

--- presentation/src/main/java/org/gdsc/presentation/view/custom/JmtSearchEditText.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/custom/JmtSearchEditText.kt
@@ -5,8 +5,11 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import org.gdsc.presentation.R
 import org.gdsc.presentation.databinding.JmtSearchEditTextBinding
+import org.gdsc.presentation.utils.addAfterTextChangedListener
 
 @SuppressLint("ClickableViewAccessibility")
 class JmtSearchEditText(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
@@ -33,6 +36,35 @@ class JmtSearchEditText(context: Context, attrs: AttributeSet) : ConstraintLayou
 
         removeAllViews()
         addView(binding.root)
+
+        setIconAndCancelVisibility()
+        setCancel()
+
+    }
+    private fun setIconAndCancelVisibility() {
+        binding.searchEditText.addAfterTextChangedListener {
+            if (text.isEmpty()) {
+                binding.searchIcon.visibility = View.VISIBLE
+                binding.cancelText.visibility = View.GONE
+            }
+            else {
+                binding.cancelText.visibility = View.VISIBLE
+                binding.searchIcon.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setCancel() {
+        binding.cancelText.setOnClickListener {
+            binding.searchEditText.text?.clear()
+            binding.searchEditText.clearFocus()
+            hideKeyboardFromEditText()
+        }
+    }
+
+    private fun hideKeyboardFromEditText() {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
     }
 
 
