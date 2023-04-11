8ef42cd180eb967dd4f64e14ec6445e9da9e98f4
soopeach <hsjeon584@gmail.com>
2023-04-11 23:49:56 +0900

    [feat/location]: meter 단위 관련 format 확장함수 구현

---
+++ domain/src/main/java/org/gdsc/domain/util.kt
@@ -0,0 +1,11 @@
+package org.gdsc.domain
+
+fun String.toMeterFormat(): String {
+    return if (this.length > 3) {
+        val km = this.substring(0, this.length - 3)
+        val m = this.substring(this.length - 3, this.length)
+        "$km.$m km"
+    } else {
+        "$this m"
+    }
+}
