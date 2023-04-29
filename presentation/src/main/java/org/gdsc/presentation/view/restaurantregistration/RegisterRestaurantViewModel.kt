1d0dc6d2937df0b219bbfeb511a59f7612b6ccb0
soopeach <hsjeon584@gmail.com>
2023-04-30 01:32:40 +0900

    [feat/register_state]: 이미지 선택 버튼 애니메이션 구현

--- presentation/src/main/java/org/gdsc/presentation/view/restaurantregistration/RegisterRestaurantViewModel.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/restaurantregistration/RegisterRestaurantViewModel.kt
@@ -1,9 +1,12 @@
 package org.gdsc.presentation.view.restaurantregistration
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,12 +18,31 @@ class RegisterRestaurantViewModel @Inject constructor() : ViewModel() {
     private var _introductionTextState = MutableStateFlow("")
     val introductionTextState = _introductionTextState.asStateFlow()
 
+    private var _isImageButtonExtended = MutableStateFlow(true)
+    val isImageButtonExtended = _isImageButtonExtended.asStateFlow()
+
+    private var _isImageButtonAnimating = MutableStateFlow(false)
+    val isImageButtonAnimating = _isImageButtonAnimating.asStateFlow()
+
     fun setDrinkPossibilityState() {
         _drinkPossibilityState.value = _drinkPossibilityState.value.not()
     }
 
     fun setIntroductionTextState(text: String) {
         _introductionTextState.value = text
+    }
+
+    fun setIsImageButtonExtended(isExtended: Boolean) {
+        _isImageButtonExtended.value = isExtended
+        setImageButtonAnimatingTime()
+    }
+
+    private fun setImageButtonAnimatingTime(animationTime: Long = 300L) {
+        viewModelScope.launch {
+            _isImageButtonAnimating.value = true
+            delay(animationTime)
+            _isImageButtonAnimating.value = false
+        }
     }
 
 }
