b81eeea7a35ee1414bc47377c75a98c150dfa6c9
soopeach <hsjeon584@gmail.com>
2023-05-22 15:18:07 +0900

    [feat/my_page_pager]: 마이 페이지 기본 Pager 및 Tab 설정

---
+++ presentation/src/main/java/org/gdsc/presentation/view/mypage/adapter/MyPagePagerAdapter.kt
@@ -0,0 +1,27 @@
+package org.gdsc.presentation.view.mypage.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import org.gdsc.presentation.view.mypage.LikedRestaurantFragment
+import org.gdsc.presentation.view.mypage.MyReviewFragment
+import org.gdsc.presentation.view.mypage.RegisteredRestaurantFragment
+
+class MyPagePagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount() = MY_PAGE_PAGER_SIZE
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            REGISTERED_RESTAURANT -> RegisteredRestaurantFragment()
+            LIKED_RESTAURANT -> LikedRestaurantFragment()
+            else -> MyReviewFragment()
+        }
+    }
+
+    companion object {
+        private const val MY_PAGE_PAGER_SIZE = 3
+
+        const val REGISTERED_RESTAURANT = 0
+        const val LIKED_RESTAURANT = 1
+        const val MY_REVIEW = 2
+    }
+}
