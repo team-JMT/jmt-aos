c3c98d542755c4ba8cd9c67370bb75a229c9aa7f
soopeach <hsjeon584@gmail.com>
2023-06-21 17:29:18 +0900

    [feat/post_nickname]: accessToken을 위한 Interceptor 구현

---
+++ data/src/main/java/org/gdsc/data/network/Interceptors.kt
@@ -0,0 +1,37 @@
+package org.gdsc.data.network
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import org.gdsc.domain.repository.TokenManager
+import java.io.IOException
+import javax.inject.Inject
+
+// reference: https://github.com/square/okhttp/issues/7164
+@Suppress("potential danger of runBlocking")
+interface CoroutineInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        return runBlocking {
+            try {
+                interceptSuspend(chain)
+            } catch (ce: CancellationException) {
+                throw IOException(ce)
+            }
+        }
+    }
+    suspend fun interceptSuspend(chain: Interceptor.Chain): Response
+}
+
+class AuthInterceptor @Inject constructor(
+    private val tokenManager: TokenManager
+) : CoroutineInterceptor {
+
+    override suspend fun interceptSuspend(chain: Interceptor.Chain): Response {
+        val token = "Bearer ${tokenManager.getAccessToken()}"
+        val newRequest = chain.request().newBuilder()
+            .addHeader("Authorization", token)
+            .build()
+        return chain.proceed(newRequest)
+    }
+}
