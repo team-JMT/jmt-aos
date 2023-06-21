68e3d6d0a7581f02f97258f035596aec7ad6e887
soopeach <hsjeon584@gmail.com>
2023-06-21 15:00:48 +0900

    [feat/manage_token]: 토큰 관련 기능들 구현

---
+++ domain/src/main/java/org/gdsc/domain/repository/TokenManager.kt
@@ -0,0 +1,17 @@
+package org.gdsc.domain.repository
+
+import org.gdsc.domain.model.TokenResponse
+
+interface TokenManager {
+
+    suspend fun saveTokenInfo(tokenResponse: TokenResponse)
+
+    suspend fun getAccessToken(): String
+
+    suspend fun getAccessTokenExpiresIn(): Long
+
+    suspend fun getGrantType(): String
+
+    suspend fun getRefreshToken(): String
+    
+}
