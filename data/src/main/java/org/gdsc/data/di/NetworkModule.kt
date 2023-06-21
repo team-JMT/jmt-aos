8db52d3b3ba67869c73ece52171d29337bb99efc
soopeach <hsjeon584@gmail.com>
2023-06-21 17:30:07 +0900

    [feat/post_nickname]: 닉네임 등록/수정 UseCase 구현

--- data/src/main/java/org/gdsc/data/di/NetworkModule.kt
+++ data/src/main/java/org/gdsc/data/di/NetworkModule.kt
@@ -6,8 +6,11 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.gdsc.data.network.AuthInterceptor
 import org.gdsc.data.network.LoginAPI
 import org.gdsc.data.network.RestaurantAPI
+import org.gdsc.data.network.UserAPI
+import org.gdsc.domain.repository.TokenManager
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,32 +19,66 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
+
+    @NormalClient
     @Provides
     @Singleton
     fun provideApiClient(): Retrofit {
 
-        val clientBuilder = OkHttpClient.Builder()
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-        clientBuilder.addInterceptor(loggingInterceptor)
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                baseClientBuilder.build()
+            ).build()
+    }
+
+    @AuthClient
+    @Provides
+    @Singleton
+    fun provideAuthorizedApiClient(tokenManager: TokenManager): Retrofit {
 
         return Retrofit
             .Builder()
-            .baseUrl("http://13.209.81.126:8080/")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(clientBuilder.build()).build()
+            .client(
+                baseClientBuilder
+                    .addInterceptor(AuthInterceptor(tokenManager))
+                    .build()
+            )
+            .build()
     }
 
     @Provides
     @Singleton
-    fun provideLoginApi(retrofit: Retrofit): LoginAPI {
+    fun provideLoginApi(@NormalClient retrofit: Retrofit): LoginAPI {
         return retrofit.create(LoginAPI::class.java)
     }
 
     @Provides
     @Singleton
-    fun provideRestaurantApi(retrofit: Retrofit): RestaurantAPI {
+    fun provideRestaurantApi(@NormalClient retrofit: Retrofit): RestaurantAPI {
         return retrofit.create(RestaurantAPI::class.java)
     }
 
+    @Provides
+    @Singleton
+    fun provideUserApi(@AuthClient retrofit: Retrofit): UserAPI {
+        return retrofit.create(UserAPI::class.java)
+    }
+
+    companion object {
+        private const val BASE_URL = "http://13.209.81.126:8080/"
+        private val baseClientBuilder: OkHttpClient.Builder
+            get() = run {
+                val clientBuilder = OkHttpClient.Builder()
+                val loggingInterceptor = HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+                return clientBuilder.addInterceptor(loggingInterceptor)
+            }
+
+    }
 }
