b5ab286aa1207a0a7e5c2c86374ff85b3926b3d6
soopeach <hsjeon584@gmail.com>
2023-06-21 15:30:48 +0900

    [feat/manage_token]: LoginManager의 suspendCoroutine을 cancellable하게 변경

--- presentation/src/main/java/org/gdsc/presentation/view/LoginManager.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/LoginManager.kt
@@ -6,29 +6,36 @@ import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
 import org.gdsc.presentation.R
 import com.google.firebase.auth.FirebaseAuth
-import kotlin.coroutines.suspendCoroutine
-class LoginManager {
-    val auth = FirebaseAuth.getInstance()
-    lateinit var oneTapClient: SignInClient
-    suspend fun signInIntent(activity: Activity)  = suspendCoroutine { continuation ->
-        oneTapClient = Identity.getSignInClient(activity)
-        val signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                .setSupported(true)
-                .build())
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    .setServerClientId(activity.getString(R.string.my_web_client_id))
-                    .setFilterByAuthorizedAccounts(false)
-                    .build())
-            .setAutoSelectEnabled(true)
-            .build()
+import kotlinx.coroutines.suspendCancellableCoroutine
 
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnFailureListener { continuation.resumeWith(Result.failure(it)) }
-            .addOnSuccessListener {
-                continuation.resumeWith(Result.success(it.pendingIntent))
-            }
-    }
+class LoginManager {
+
+    val auth = FirebaseAuth.getInstance()
+
+    lateinit var oneTapClient: SignInClient
+    suspend fun signInIntent(activity: Activity) =
+        suspendCancellableCoroutine { cancellableContinuation ->
+            oneTapClient = Identity.getSignInClient(activity)
+            val signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(
+                    BeginSignInRequest.PasswordRequestOptions.builder()
+                        .setSupported(true)
+                        .build()
+                )
+                .setGoogleIdTokenRequestOptions(
+                    BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        .setServerClientId(activity.getString(R.string.my_web_client_id))
+                        .setFilterByAuthorizedAccounts(false)
+                        .build()
+                )
+                .setAutoSelectEnabled(true)
+                .build()
+
+            oneTapClient.beginSignIn(signInRequest)
+                .addOnFailureListener { cancellableContinuation.resumeWith(Result.failure(it)) }
+                .addOnSuccessListener {
+                    cancellableContinuation.resumeWith(Result.success(it.pendingIntent))
+                }
+        }
 }
