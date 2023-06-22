c4310b63e5b70de008e07217afcf4749e12f4f27
soopeach <hsjeon584@gmail.com>
2023-06-22 10:24:47 +0900

    [feat/sign_up]: 프로필 이미지 파일을 서버에 업로드하는 UseCase 추가

--- data/src/main/java/org/gdsc/data/datasource/UserDataSourceImpl.kt
+++ data/src/main/java/org/gdsc/data/datasource/UserDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package org.gdsc.data.datasource
 
+import okhttp3.MultipartBody
 import org.gdsc.data.network.UserAPI
 import org.gdsc.domain.model.request.NicknameRequest
 import org.gdsc.domain.model.response.NicknameResponse
@@ -10,5 +11,9 @@ class UserDataSourceImpl @Inject constructor(
 ) : UserDataSource {
     override suspend fun postNickname(nicknameRequest: NicknameRequest): NicknameResponse {
         return userAPI.postNickname(nicknameRequest).data
+    }
+
+    override suspend fun postProfileImg(file: MultipartBody.Part): String {
+        return userAPI.postProfileImg(file).data
     }
 }
--- data/src/main/java/org/gdsc/data/network/UserAPI.kt
+++ data/src/main/java/org/gdsc/data/network/UserAPI.kt
@@ -1,13 +1,20 @@
 package org.gdsc.data.network
 
+import okhttp3.MultipartBody
 import org.gdsc.data.model.Response
 import org.gdsc.domain.model.request.NicknameRequest
 import org.gdsc.domain.model.response.NicknameResponse
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface UserAPI {
 
     @POST("api/v1/user/nickname")
     suspend fun postNickname(@Body nicknameRequest: NicknameRequest): Response<NicknameResponse>
+
+    @Multipart
+    @POST("api/v1/user/profileImg")
+    suspend fun postProfileImg(@Part profileImageFile: MultipartBody.Part): Response<String>
 }
