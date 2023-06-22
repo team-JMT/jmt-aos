053bcfbedc1f18b0e2b202ad20f3d1649b223877
soopeach <hsjeon584@gmail.com>
2023-06-22 10:24:47 +0900

    [feat/sign_up]: 프로필 이미지 파일을 서버에 업로드하는 UseCase 추가

--- domain/src/main/java/org/gdsc/domain/repository/UserRepository.kt
+++ domain/src/main/java/org/gdsc/domain/repository/UserRepository.kt
@@ -1,9 +1,12 @@
 package org.gdsc.domain.repository
 
+import okhttp3.MultipartBody
 import org.gdsc.domain.model.request.NicknameRequest
 import org.gdsc.domain.model.response.NicknameResponse
 
 interface UserRepository {
 
     suspend fun postNickname(nicknameRequest: NicknameRequest): NicknameResponse
+
+    suspend fun postProfileImg(file: MultipartBody.Part): String
 }
