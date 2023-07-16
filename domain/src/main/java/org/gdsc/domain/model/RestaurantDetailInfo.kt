46417159d21e564258852c8c330f1e92697d9868
Jim-swit <dogdduddy@gmail.com>
2023-07-16 16:18:10 +0900

    Fix : argument로 사용하는 Data Class Serializable 에러

--- domain/src/main/java/org/gdsc/domain/model/RestaurantDetailInfo.kt
+++ domain/src/main/java/org/gdsc/domain/model/RestaurantDetailInfo.kt
@@ -1,13 +1,21 @@
 package org.gdsc.domain.model
 
+import com.google.gson.annotations.SerializedName
 import okhttp3.MultipartBody
 
 data class RestaurantDetailInfo (
+    @SerializedName("introduce")
     val introduce: String,
+    @SerializedName("categoryId")
     val categoryId: Long,
+    @SerializedName("pictures")
     val pictures: List<MultipartBody.Part>,
+    @SerializedName("canDrinkLiquor")
     val canDrinkLiquor: Boolean,
+    @SerializedName("goWellWithLiquor")
     val goWellWithLiquor: String,
+    @SerializedName("recommendMenu")
     val recommendMenu: String,
+    @SerializedName("restaurantLocationAggregateIdg")
     val restaurantLocationAggregateIdg: String,
-)
+): java.io.Serializable
