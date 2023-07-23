4b57e677f96a83cfc7605a9e525d5484e2a6cf3f
Jim-swit <jim.jeon@swit.io>
2023-07-23 15:59:54 +0900

    [fix/register_restaurant]: 웹뷰 뒤로가기 구현
    - 뒤로가기 실행 시 웹뷰에 스택이 있을 시 웹뷰 뒤로가기 실행, 스택이 없을 시 Client finish 실행

--- presentation/src/main/java/org/gdsc/presentation/view/WebViewActivity.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/WebViewActivity.kt
@@ -1,11 +1,18 @@
 package org.gdsc.presentation.view
+
+import android.annotation.TargetApi
 import android.content.pm.ActivityInfo
+import android.net.http.SslError
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
+import android.webkit.PermissionRequest
+import android.webkit.SslErrorHandler
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.ActionBar
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import org.gdsc.presentation.databinding.ActivityWebViewBinding
 
@@ -18,14 +25,23 @@ class WebViewActivity : AppCompatActivity() {
         binding = ActivityWebViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        onBackPressedDispatcher.addCallback {
+            if(webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                // TODO(): 메인으로 이동 등, 페이지 이동 기능 추가
+                finish()
+            }
+        }
+
         webView = binding.webView
         webViewInit()
         intent.extras?.let {
             binding.webView.loadUrl(it.getString("url")!!)
         }
 
-        val actionBar: ActionBar? = supportActionBar
-        actionBar!!.hide()
+//        val actionBar: ActionBar? = supportActionBar
+//        actionBar!!.hide()
     }
     fun webViewInit() {
         val webSettings: WebSettings = webView.getSettings()
@@ -33,7 +49,8 @@ class WebViewActivity : AppCompatActivity() {
 
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) //화면이 계속 켜짐
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_USER
-        webView.setWebViewClient(WebViewClient())
+        webView.webViewClient = WebViewClient()
+
         webView.addJavascriptInterface(WebAppInterface(this), "Android")
     }
 }
