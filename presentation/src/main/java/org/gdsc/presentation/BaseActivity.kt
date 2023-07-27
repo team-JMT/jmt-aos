9a8c9e08f97c7b780b1dc993114ebadb59168ae6
DOGDDUDDY <dogdduddy@gmail.com>
2023-07-27 18:47:54 +0900

    [feat/mypage]: 이미지 퍼미션 체크 및 가져오는 플로우 변경

---
+++ presentation/src/main/java/org/gdsc/presentation/BaseActivity.kt
@@ -0,0 +1,7 @@
+package org.gdsc.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity() {
+
+}
--- presentation/src/main/java/org/gdsc/presentation/login/SignUpCompleteFragment.kt
+++ presentation/src/main/java/org/gdsc/presentation/login/SignUpCompleteFragment.kt
@@ -1,25 +1,37 @@
 package org.gdsc.presentation.login
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import org.gdsc.presentation.databinding.FragmentSignUpCompleteBinding
+import org.gdsc.presentation.utils.checkMediaPermissions
 import org.gdsc.presentation.utils.findPath
+import org.gdsc.presentation.utils.showMediaPermissionsDialog
 import org.gdsc.presentation.view.MainActivity
 import java.io.File
 
@@ -31,7 +43,21 @@ class SignUpCompleteFragment : Fragment() {
         get() = requireNotNull(_binding)
 
     private val viewModel: LoginViewModel by activityViewModels()
-    private val args by navArgs<SignUpCompleteFragmentArgs>()
+
+    private val onSuccess: () -> Unit = {
+        val action = SignUpCompleteFragmentDirections.actionSignUpCompleteFragmentToImagepickerFragment()
+        findNavController().navigate(action)
+    }
+
+    private val requestPermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { isGranted ->
+        if(isGranted.all{ it.value }) {
+            onSuccess.invoke()
+        }else{
+            this.showMediaPermissionsDialog()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,35 +71,33 @@ class SignUpCompleteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setProfileImageButtonAnimation()
 
-        args.imageUri?.let {
-            Glide.with(binding.root)
-                .load(it.toUri())
-                .centerCrop()
-                .into(binding.profileImage)
-
-            binding.profileImage.clipToOutline = true
+        setFragmentResultListener("pickImage") { _, bundle ->
+            val imageUri = bundle.getString("imageUri")
+            imageUri?.let {
+                viewModel.updateProfileImageState(it)
+            }
         }
 
         binding.nicknameText.text = viewModel.nicknameState.value
 
         binding.profileImageAddButton.setOnClickListener {
-            val action =
-                SignUpCompleteFragmentDirections.actionSignUpCompleteFragmentToPermissionFragment()
-            findNavController().navigate(action)
+            this.checkMediaPermissions(
+                requestPermissionsLauncher
+            ) { onSuccess.invoke() }
         }
 
         binding.nextBtn.setOnClickListener {
-            args.imageUri?.let {
-                val file = File(it.toUri().findPath(requireContext()))
-                val requestFile = RequestBody.create(MediaType.parse("image/png"), file)
-                val body =
-                    MultipartBody.Part.createFormData("profileImg", file.name, requestFile)
-                viewModel.requestSignUpWithImage(body!!) {
-                    moveToMain()
-                }
-            }?: run{
-                viewModel.requestSignUpWithoutImage() {
-                    moveToMain()
+            viewModel.profileImageState.value.let {
+                if(it.isNullOrEmpty())
+                    viewModel.requestSignUpWithoutImage { moveToMain() }
+                else {
+                    val file = File(it.toUri().findPath(requireContext()))
+                    val requestFile = RequestBody.create(MediaType.parse("image/png"), file)
+                    val body =
+                        MultipartBody.Part.createFormData("profileImg", file.name, requestFile)
+                    viewModel.requestSignUpWithImage(body) {
+                        moveToMain()
+                    }
                 }
             }
         }
