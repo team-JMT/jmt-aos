f9bd3f47ca6f4af1265b34ba04a621806d9399b4
DOGDDUDDY <dogdduddy@gmail.com>
2023-07-27 18:49:12 +0900

    [feat/mypage]: 유저 프로필이미지 변경

--- presentation/src/main/java/org/gdsc/presentation/view/mypage/SettingsFragment.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/mypage/SettingsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
@@ -13,14 +14,19 @@ import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import org.gdsc.domain.Empty
 import org.gdsc.domain.model.UserInfo
 import org.gdsc.presentation.R
 import org.gdsc.presentation.databinding.FragmentSettingsBinding
+import org.gdsc.presentation.utils.findPath
 import org.gdsc.presentation.utils.repeatWhenUiStarted
 import org.gdsc.presentation.view.MainActivity
 import org.gdsc.presentation.view.custom.JmtSnackbar
 import org.gdsc.presentation.view.mypage.viewmodel.MyPageViewModel
+import java.io.File
 
 @AndroidEntryPoint
 class SettingsFragment: Fragment() {
@@ -30,8 +36,6 @@ class SettingsFragment: Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: MyPageViewModel by viewModels()
-
-    private var user: UserInfo = UserInfo()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,23 +49,31 @@ class SettingsFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initUserInfo()
         setToolbarTitle()
-
-        viewLifecycleOwner.repeatWhenUiStarted {
-            viewModel.getUserInfo()
-        }
-
-        viewLifecycleOwner.repeatWhenUiStarted {
-            viewModel.userInfoState.collect {
-                user = it
-                initUserInfo()
-            }
-        }
+        observeState()
 
         binding.profileImageButton.setOnClickListener {
-            setFragmentResultListener("pickImages") { _, bundle ->
-                val result = bundle.getStringArray("imagesUri")
+            setFragmentResultListener("pickImage") { _, bundle ->
+                val result = bundle.getString("imageUri")
+                result?.let {
+                    val file = File(it.toUri().findPath(requireContext()))
+                    val requestFile = RequestBody.create(MediaType.parse("image/png"), file)
+                    val body =
+                        MultipartBody.Part.createFormData("profileImg", file.name, requestFile)
+
+                    viewModel.updateProfileImage(body) {
+                        viewModel.updateProfileImageState(it)
+                    }
+                }?: run {
+                    viewModel.updateDefaultProfileImage {
+                        viewModel.updateProfileImageState(String.Empty)
+                    }
+                }
             }
+
+            val navigate = SettingsFragmentDirections.actionSettingsFragmentToImagePickerFragment()
+            findNavController().navigate(navigate)
         }
 
         binding.btnChangeUserName.setOnClickListener {
@@ -91,16 +103,37 @@ class SettingsFragment: Fragment() {
     }
 
     private fun initUserInfo() {
+        repeatWhenUiStarted {
+            viewModel.getUserInfo()
+        }
+    }
+
+    private fun observeState() {
+        repeatWhenUiStarted {
+            viewModel.nicknameState.collect {
+                binding.tvUserName.text = it
+            }
+        }
+
+        repeatWhenUiStarted {
+            viewModel.profileImageState.collect {
+                initProfileImage(it)
+            }
+        }
+
+        repeatWhenUiStarted {
+            viewModel.emailState.collect {
+                binding.tvUserEmal.text = it
+            }
+        }
+
+    }
+
+    private fun initProfileImage(profileImg: String) {
         Glide.with(this)
-            .load(
-                if(user.profileImg.isNullOrEmpty()) user.profileImg
-                else user.profileImg
-            )
+            .load(profileImg)
             .placeholder(R.drawable.base_profile_image)
             .into(binding.profileImage)
-
-        binding.tvUserName.text = user.nickname
-        binding.tvUserEmal.text = user.email
     }
 
     override fun onDestroyView() {
--- presentation/src/main/java/org/gdsc/presentation/view/mypage/viewmodel/MyPageViewModel.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/mypage/viewmodel/MyPageViewModel.kt
@@ -5,14 +5,19 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import okhttp3.MultipartBody
+import org.gdsc.domain.Empty
 import org.gdsc.domain.SortType
 import org.gdsc.domain.model.UserInfo
 import org.gdsc.domain.model.response.NicknameResponse
 import org.gdsc.domain.usecase.CheckDuplicatedNicknameUseCase
 import org.gdsc.domain.usecase.PostNicknameUseCase
 import org.gdsc.domain.usecase.user.GetUserInfoUseCase
+import org.gdsc.domain.usecase.user.PostDefaultProfileImageUseCase
+import org.gdsc.domain.usecase.user.PostProfileImageUseCase
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,23 +25,40 @@ class MyPageViewModel @Inject constructor(
     private val userInfoUseCase: GetUserInfoUseCase,
     private val postNicknameUseCase: PostNicknameUseCase,
     private val checkDuplicatedNicknameUseCase: CheckDuplicatedNicknameUseCase,
+    private val postProfileImageUseCase: PostProfileImageUseCase,
+    private val postDefaultProfileImageUseCase: PostDefaultProfileImageUseCase,
 ): ViewModel() {
 
     private var _sortTypeState = MutableStateFlow(SortType.INIT)
     val sortTypeState: StateFlow<SortType>
         get() = _sortTypeState
 
-    private var _userInfoState = MutableStateFlow(UserInfo())
-    val userInfoState: StateFlow<UserInfo>
-        get() = _userInfoState
+    private var _nicknameState = MutableStateFlow(String.Empty)
+    val nicknameState = _nicknameState.asStateFlow()
+
+    private var _profileImageState = MutableStateFlow(String.Empty)
+    val profileImageState = _profileImageState.asStateFlow()
+
+    private var _emailState = MutableStateFlow(String.Empty)
+    val emailState = _emailState.asStateFlow()
 
     suspend fun getUserInfo() {
         return withContext(viewModelScope.coroutineContext) {
             val response = userInfoUseCase.invoke()
-            _userInfoState.value = response
+
+            _nicknameState.value = response.nickname
+            _profileImageState.value = response.profileImg
+            _emailState.value = response.email
         }
     }
 
+    fun updateNickNameState(nickName: String) {
+        _nicknameState.value = nickName
+    }
+
+    fun updateProfileImageState(profileImage: String) {
+        _profileImageState.value = profileImage
+    }
 
     fun updateUserName(nickName: String, afterSuccessCallback: (response: NicknameResponse) -> Unit) {
         viewModelScope.launch {
@@ -44,6 +66,8 @@ class MyPageViewModel @Inject constructor(
             afterSuccessCallback(response)
         }
     }
+
+
 
     fun checkDuplicatedNickname(
         nickName: String,
@@ -58,6 +82,20 @@ class MyPageViewModel @Inject constructor(
                     onNicknameIsDuplicated()
                 }
             }
+        }
+    }
+
+    fun updateProfileImage(file: MultipartBody.Part, afterSuccessSignUp: () -> Unit) {
+        viewModelScope.launch {
+            postProfileImageUseCase(file)
+            afterSuccessSignUp()
+        }
+    }
+
+    fun updateDefaultProfileImage(afterSuccessSignUp: () -> Unit) {
+        viewModelScope.launch {
+            postDefaultProfileImageUseCase()
+            afterSuccessSignUp()
         }
     }
 }
