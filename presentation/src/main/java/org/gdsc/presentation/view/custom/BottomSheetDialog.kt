15dbe1b14385564cd0a7982f191fc16886db0dae
DOGDDUDDY <dogdduddy@gmail.com>
2023-08-05 15:45:08 +0900

    [feat/mypage_image]: BottomSheetDialog 커스텀뷰 추가

---
+++ presentation/src/main/java/org/gdsc/presentation/view/custom/BottomSheetDialog.kt
@@ -0,0 +1,59 @@
+package org.gdsc.presentation.view.custom
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import dagger.hilt.android.internal.managers.FragmentComponentManager
+import org.gdsc.presentation.R
+
+class BottomSheetDialog {
+
+    private var dismissAfterClick = true
+    private var btmDlg: BottomSheetDialog
+    var customView: View? = null
+
+    var style: Int = R.style.JmtBottomSheetStyle
+    private var context: Context
+
+
+    constructor(context: Context) {
+        this.context = context
+        btmDlg = BottomSheetDialog(context, style).apply {
+            init()
+        }
+    }
+
+    fun BottomSheetDialog.init() {
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        behavior.skipCollapsed = true
+        behavior.setPeekHeight(0, false)
+        behavior.maxHeight = getBottomSheetDialogDefaultHeight()
+    }
+
+    fun <T: ViewBinding> bindBuilder(binding: T, callback: T.(BottomSheetDialog) -> Unit) {
+        this.customView = binding.root
+        btmDlg.setContentView(this.customView!!)
+        binding.callback(btmDlg)
+    }
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getScreenRealHeight() * 70 / 100
+    }
+
+    fun setCustomViewHeightMax() {
+        val height = getScreenRealHeight()
+
+        btmDlg.behavior.maxHeight = height
+        this.customView?.layoutParams?.height = height
+    }
+
+    private fun getScreenRealHeight(): Int {
+        val activity = FragmentComponentManager.findActivity(context) as Activity
+        return activity
+            .window.decorView.findViewById<ViewGroup>(android.R.id.content)
+            .getChildAt(0).height
+    }
+}
