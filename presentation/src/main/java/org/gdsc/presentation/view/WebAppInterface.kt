5efa64fead9630617fad8c64b785526bc86960fe
DOGDDUDDY <dogdduddy@gmail.com>
2023-09-03 19:05:50 +0900

    [feat/webview_bridge]: Route 구현
    - Route 데이터에 맞춰 해당 페이지로 이동
    - FE 미배포로 디테일한 내용은 추후 구현

--- presentation/src/main/java/org/gdsc/presentation/view/WebAppInterface.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/WebAppInterface.kt
@@ -3,6 +3,8 @@ package org.gdsc.presentation.view
 import android.content.Context
 import android.webkit.JavascriptInterface
 import android.widget.Toast
+import org.gdsc.presentation.model.Route
+import org.json.JSONObject
 
 /** Instantiate the interface and set the context  */
 class WebAppInterface(
@@ -14,25 +16,13 @@ class WebAppInterface(
 ) {
 
     /** Show a toast from the web page  */
-    @JavascriptInterface
-    fun showToast(toast: String) {
-        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
-    }
 
     @JavascriptInterface
-    fun navigation(isVisible: Boolean) {
+    fun navigation(data: String) {
+        val isVisible:Boolean = JSONObject(data).get("isVisible") as Boolean
+
         if (isVisible) slideUpBottomNavigationView()
         else slideDownBottomNavigationView()
-    }
-
-    @JavascriptInterface
-    fun editRestaurantInfo(restaurantId: Int) {
-        navigateToRestaurantEdit(restaurantId)
-    }
-
-    // webView.canGoBack으로 뒤로가기 처리 완료해서 비워뒀습니다.
-    @JavascriptInterface
-    fun back(isEnableBack: Boolean) {
     }
 
     @JavascriptInterface
@@ -43,7 +33,17 @@ class WebAppInterface(
     }
 
     @JavascriptInterface
-    fun navigate(route: String) {
+    fun navigate(data: String) {
+        when(JSONObject(data).get("route") as String) {
+            Route.EDIT_RESTAURANT.route-> {
+                val restaurantId = JSONObject(data).get("restaurantId") as Int
+                navigateToRestaurantEdit(restaurantId)
+            }
+        }
     }
 
+    // webView.canGoBack으로 뒤로가기 처리 완료해서 비워뒀습니다.
+    @JavascriptInterface
+    fun back(isEnableBack: Boolean) {
+    }
 }
