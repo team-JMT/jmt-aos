60765dcc11d15c5a2f78d43aff5657ca46d5856d
soopeach <hsjeon584@gmail.com>
2023-09-05 09:09:20 +0900

    [feat/bitmap_resizing]: 프로필 이미지 등록할 때 이미지 크기 조절 적용

--- presentation/src/main/java/org/gdsc/presentation/login/SignUpCompleteFragment.kt
+++ presentation/src/main/java/org/gdsc/presentation/login/SignUpCompleteFragment.kt
@@ -1,40 +1,26 @@
 package org.gdsc.presentation.login
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.Glide
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import org.gdsc.presentation.BaseFragment
 import org.gdsc.presentation.databinding.FragmentSignUpCompleteBinding
+import org.gdsc.presentation.utils.BitmapUtils.getCompressedBitmapFromUri
+import org.gdsc.presentation.utils.BitmapUtils.saveBitmapToFile
 import org.gdsc.presentation.utils.checkMediaPermissions
-import org.gdsc.presentation.utils.findPath
-import org.gdsc.presentation.utils.showMediaPermissionsDialog
 import org.gdsc.presentation.view.MainActivity
-import java.io.File
 
 
 class SignUpCompleteFragment : BaseFragment() {
@@ -79,15 +65,22 @@ class SignUpCompleteFragment : BaseFragment() {
 
         binding.nextBtn.setOnClickListener {
             viewModel.profileImageState.value.let {
-                if(it.isNullOrEmpty())
+                if(it.isEmpty())
                     viewModel.requestSignUpWithoutImage { moveToMain() }
                 else {
-                    val file = File(it.toUri().findPath(requireContext()))
-                    val requestFile = RequestBody.create(MediaType.parse("image/png"), file)
-                    val body =
-                        MultipartBody.Part.createFormData("profileImg", file.name, requestFile)
-                    viewModel.requestSignUpWithImage(body) {
-                        moveToMain()
+                    val file = it.toUri()
+                        .getCompressedBitmapFromUri(requireContext())
+                        ?.saveBitmapToFile(requireContext(), "profile.jpg")
+
+                    if (file != null) {
+                        val requestFile = RequestBody.create(MediaType.parse("image/png"), file)
+                        val body =
+                            MultipartBody.Part.createFormData("profileImg", file.name, requestFile)
+                        viewModel.requestSignUpWithImage(body) {
+                            moveToMain()
+                        }
+                    } else {
+                        // TODO: Exception Handling
                     }
                 }
             }
