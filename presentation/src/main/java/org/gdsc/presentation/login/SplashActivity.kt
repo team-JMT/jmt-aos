6de9c9624267d54e930ecb7753385bb78a1593fd
soopeach <hsjeon584@gmail.com>
2023-09-10 16:32:47 +0900

    [feat/auto_login]: 자동 로그인 기능 구현(Splash에서 확인 후 처리)

--- presentation/src/main/java/org/gdsc/presentation/login/SplashActivity.kt
+++ presentation/src/main/java/org/gdsc/presentation/login/SplashActivity.kt
@@ -5,12 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import org.gdsc.domain.usecase.token.GetRefreshTokenUseCase
+import org.gdsc.domain.usecase.token.PostRefreshTokenUseCase
+import org.gdsc.domain.usecase.token.VerifyAccessTokenUseCase
 import org.gdsc.presentation.R
+import org.gdsc.presentation.view.MainActivity
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
 
     val handle: Handler = Handler(Looper.getMainLooper())
+
+    @Inject
+    lateinit var verifyAccessTokenUseCase: VerifyAccessTokenUseCase
+
+    @Inject
+    lateinit var postRefreshTokenUseCase: PostRefreshTokenUseCase
+
+    @Inject
+    lateinit var getRefreshTokenUseCase: GetRefreshTokenUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +35,37 @@ class SplashActivity : AppCompatActivity() {
         setToFullPage()
 
         handle.postDelayed({
-            Intent(this, LoginActivity::class.java).apply {
-                startActivity(this)
+
+            lifecycleScope.launch {
+                val isAccessTokenValid = verifyAccessTokenUseCase.invoke()
+
+                // 정상이면 그냥 리프레쉬
+                if (isAccessTokenValid) {
+                    postRefreshTokenUseCase.invoke(getRefreshTokenUseCase.invoke())
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                // access Token이 만료되었다면
+                } else {
+                    // refresh 시도
+                    val isRefreshSuccess = postRefreshTokenUseCase.invoke(getRefreshTokenUseCase.invoke())
+
+                    // refresh 성공했다면
+                    if (isRefreshSuccess) {
+                        val intent = Intent(applicationContext, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    // refresh 실패했다면
+                    } else {
+                        val intent = Intent(applicationContext, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                }
             }
 
-            finish()
         }, 2000)
     }
 
@@ -30,6 +73,7 @@ class SplashActivity : AppCompatActivity() {
         val window = window
         window.setFlags(
             android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
