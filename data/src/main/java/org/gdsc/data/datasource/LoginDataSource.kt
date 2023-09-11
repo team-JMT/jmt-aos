67e8ea5b891060e3a5251380457ea50b424f59a3
soopeach <hsjeon584@gmail.com>
2023-09-11 18:36:45 +0900

    [feat/jmtException_sandbox]: 구글 로그인 부분에 에러 핸들링 코드 적용

--- data/src/main/java/org/gdsc/data/datasource/LoginDataSource.kt
+++ data/src/main/java/org/gdsc/data/datasource/LoginDataSource.kt
@@ -1,9 +1,10 @@
 package org.gdsc.data.datasource
 
+import org.gdsc.domain.model.Response
 import org.gdsc.domain.model.response.TokenResponse
 
 interface LoginDataSource {
-    suspend fun postSignUpWithGoogleToken(token: String): TokenResponse
+    suspend fun postSignUpWithGoogleToken(token: String): Result<Response<TokenResponse>>
 
     suspend fun postAppleToken(email: String, clientId: String): TokenResponse
 
