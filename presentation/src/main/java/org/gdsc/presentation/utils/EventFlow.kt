5c54ae2cac77e2c388628b47934cc3b0d3f491db
soopeach <hsjeon584@gmail.com>
2023-09-12 12:27:18 +0900

    [feat/jmtException_sandbox]: EventFlow 구현

---
+++ presentation/src/main/java/org/gdsc/presentation/utils/EventFlow.kt
@@ -0,0 +1,45 @@
+package org.gdsc.presentation.utils
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.MutableSharedFlow
+import org.gdsc.presentation.utils.EventFlow.Companion.DEFAULT_REPLAY
+import java.util.concurrent.atomic.AtomicBoolean
+
+interface EventFlow<out T> : Flow<T> {
+
+    companion object {
+        const val DEFAULT_REPLAY = 2
+    }
+}
+
+interface MutableEventFlow<T> : EventFlow<T>, FlowCollector<T>
+
+private class EventFlowSlot<T>(val value: T) {
+
+    private val consumed = AtomicBoolean(false)
+
+    fun markConsumed() = consumed.getAndSet(true)
+}
+
+private class EventFlowImpl<T>(replay: Int) : MutableEventFlow<T> {
+
+    private val flow: MutableSharedFlow<EventFlowSlot<T>> = MutableSharedFlow(replay)
+
+    override suspend fun collect(collector: FlowCollector<T>) =
+        flow.collect { slot ->
+            if (slot.markConsumed().not()) {
+                collector.emit(slot.value)
+            }
+        }
+
+    override suspend fun emit(value: T) {
+        flow.emit(EventFlowSlot(value))
+    }
+}
+
+private class ReadOnlyEventFlow<T>(flow: EventFlow<T>) : EventFlow<T> by flow
+
+fun <T> MutableEventFlow(replay: Int = DEFAULT_REPLAY): MutableEventFlow<T> = EventFlowImpl(replay)
+
+fun <T> MutableEventFlow<T>.asEventFlow(): EventFlow<T> = ReadOnlyEventFlow(this)
