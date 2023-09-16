b81442e11985e7176f6502fcfaa7f862895694c8
DOGDDUDDY <dogdduddy@gmail.com>
2023-09-16 20:32:18 +0900

    [feat/mypage_registered]: Stetho 적용

--- app/src/main/java/org/gdsc/jmt/App.kt
+++ app/src/main/java/org/gdsc/jmt/App.kt
@@ -1,9 +1,15 @@
 package org.gdsc.jmt
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) Stetho.initializeWithDefaults(this)
+    }
 }
