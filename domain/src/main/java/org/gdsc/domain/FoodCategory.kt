6856b64fdfb013aa55f4bac8f617e57ae3fb44b3
soopeach <hsjeon584@gmail.com>
2023-09-26 15:46:15 +0900

    [fix/register_restaurant_design]: 퓨전 카테고리 추가

--- domain/src/main/java/org/gdsc/domain/FoodCategory.kt
+++ domain/src/main/java/org/gdsc/domain/FoodCategory.kt
@@ -5,9 +5,10 @@ enum class FoodCategory(val id: Long, val text: String, val key:String) {
     JAPANESE(2L, "일식", "JAPAN"),
     CHINESE(3L, "중식", "CHINA"),
     WESTERN(4L, "양식", "FOREIGN"),
-    CAFE(5L, "카페", "CAFE"),
-    BAR(6L, "주점", "BAR"),
-    ETC(7L, "기타", "ETC"),
+    FUSION(5L, "퓨전", "FUSION"),
+    CAFE(6L, "카페", "CAFE"),
+    BAR(7L, "주점", "BAR"),
+    ETC(8L, "기타", "ETC"),
     INIT(-1L, "(필수) 어떤 종류의 식당인가요?", "");
 
     companion object {
@@ -17,6 +18,7 @@ enum class FoodCategory(val id: Long, val text: String, val key:String) {
                 JAPANESE.text,
                 CHINESE.text,
                 WESTERN.text,
+                FUSION.text,
                 CAFE.text,
                 BAR.text,
                 ETC.text
