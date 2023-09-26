aed39efc12a8bc66eeb7e8f6b67147279815b85b
DOGDDUDDY <dogdduddy@gmail.com>
2023-09-26 14:46:05 +0900

    [fix/confirm_restaurant_design]: JMT-236 좋아한 맛집 숨김 처리

--- presentation/src/main/java/org/gdsc/presentation/view/mypage/LikedRestaurantFragment.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/mypage/LikedRestaurantFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,6 +39,7 @@ class LikedRestaurantFragment : Fragment() {
         observeState()
         setSpinners()
 
+        binding.notYetLayout.root.isVisible = true
     }
 
     private fun setSpinners() {
