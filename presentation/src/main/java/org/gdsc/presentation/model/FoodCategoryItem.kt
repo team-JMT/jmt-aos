f420c5d46bf80d16163d04769ec1d8e1baf6aff7
swit-jim <jim.jeon@swit.io>
2024-03-10 12:22:15 +0900

    [feat/naver_map_marker]: Marker Icon 변경 및 반영

--- presentation/src/main/java/org/gdsc/presentation/model/FoodCategoryItem.kt
+++ presentation/src/main/java/org/gdsc/presentation/model/FoodCategoryItem.kt
@@ -25,4 +25,18 @@ data class FoodCategoryItem(
             else -> R.drawable.ic_etc
         }
     }
+
+    @DrawableRes fun getMarkerIcon(): Int {
+        return when(this.categoryItem) {
+            FoodCategory.KOREAN -> R.drawable.ic_marker_korea
+            FoodCategory.JAPANESE -> R.drawable.ic_marker_japan
+            FoodCategory.CHINESE -> R.drawable.ic_marker_china
+            FoodCategory.WESTERN -> R.drawable.ic_marker_foreign
+            FoodCategory.FUSION -> R.drawable.ic_marker_fusion
+            FoodCategory.CAFE -> R.drawable.ic_marker_cafe
+            FoodCategory.BAR -> R.drawable.ic_marker_bar
+            FoodCategory.ETC -> R.drawable.ic_marker_etc
+            else -> R.drawable.ic_marker_etc
+        }
+    }
 }
