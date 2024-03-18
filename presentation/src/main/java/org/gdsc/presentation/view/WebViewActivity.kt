8a1afb7917cda97f92358d3db6945fc85fa1f902
swit-jim <jim.jeon@swit.io>
2024-03-18 22:22:58 +0900

    [fix/webview_bridge_token]: WebViewActivity token Setting

--- presentation/src/main/java/org/gdsc/presentation/view/WebViewActivity.kt
+++ presentation/src/main/java/org/gdsc/presentation/view/WebViewActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.ActivityInfo
 import android.net.http.SslError
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import android.webkit.PermissionRequest
 import android.webkit.SslErrorHandler
@@ -13,14 +14,23 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.addCallback
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import org.gdsc.presentation.databinding.ActivityWebViewBinding
 import org.gdsc.presentation.utils.repeatWhenUiStarted
+import org.gdsc.presentation.view.webview.SpecificWebViewViewModel
 
-
+@AndroidEntryPoint
 class WebViewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWebViewBinding
     private lateinit var webView: WebView
+
+    private val specificWebViewViewModel: SpecificWebViewViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWebViewBinding.inflate(layoutInflater)
@@ -55,7 +65,14 @@ class WebViewActivity : AppCompatActivity() {
             settings.javaScriptEnabled = true // allow the js
             settings.domStorageEnabled = true
             webViewClient = WebViewClient()
-            addJavascriptInterface(WebAppInterface(this@WebViewActivity), "webviewBridge")
+            addJavascriptInterface(WebAppInterface(this@WebViewActivity,
+                setAccessToken = {
+                    lifecycleScope.launch {
+                        binding.webView.loadUrl(
+                            "javascript:setAccessToken('${specificWebViewViewModel.getAccessToken()}')"
+                        )
+                    }
+                }), "webviewBridge")
         }
 
 
